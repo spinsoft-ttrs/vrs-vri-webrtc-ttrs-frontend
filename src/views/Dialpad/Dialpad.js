eaff5e92b2fa26af8ca186ab78ea64e7fa1541a3
kasemsan00 <106854077+kasemsan00@users.noreply.github.com>
2023-05-11 23:41:28 +0700

    dev

--- src/views/Dialpad/Dialpad.js
+++ src/views/Dialpad/Dialpad.js
@@ -101,9 +101,12 @@ const Dialpad = () => {
       if (browser.os === "Android OS") {
         alert("อุปกรณ์ไม่รองรับการใช้งาน");
         window.location.href = "https://play.google.com/store/apps/details?id=ttrs.vrs&hl=en_GB&gl=th";
-      } else if (browser.os === "iOS") {
+        return;
+      }
+      if (browser.os === "iOS") {
         alert("อุปกรณ์ไม่รองรับการใช้งาน");
         window.location.href = "https://apps.apple.com/th/app/ttrs-video/id1467360876?l=th";
+        return;
       }
       // }
     }
